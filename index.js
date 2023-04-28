95a1674e8f158f4a235dabce9c4f60a237fb4bb5
KeltonSterett <keltonsterett24@gmail.com>
2023-04-27 23:28:03 -0600

    used fs module to write to logo.svg, tested all shapes, added conditional for user input and shape selection

--- index.js
+++ index.js
@@ -5,6 +5,37 @@ const fs = require('fs');
 // importing class from shapes.js
 const {Square, Triangle, Circle} = require('./lib/shapes.js');
 
+// function to write to file using 'fs' module from npm to use inquirer answers to create logo
+function writeToFile(fileName, answers) {
+    let svgString = "";
+    // set width and height of logo
+    svgString = 
+    '<svg version="1.1"  width="300" height="300" xmlns="http://www.w3.org/2000/svg">';
+    
+    svgString += "<g>"; 
+
+    svgString += `${answers.shape}`;
+
+    let shapeInput;
+    if (answers.shape === "square") {
+        shapeInput = new Square();
+        svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeColor}"/>`;
+    } else if (answers.shape === "circle") {
+        shapeInput = new Circle();
+        svgString += `<circle cx="150" cy="150" r="80" fill="${answers.shapeColor}"/>`;
+    } else if (answers.shape === "triangle") {
+        shapeInput = new Triangle();
+        svgString += `<polygon points="150,40 73,200 227,200" fill="${answers.shapeColor}"/>`;
+    }
+
+    svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>` ;
+    svgString += "</g>";
+    svgString += "</svg>";
+
+    fs.writeFile(fileName, svgString, (err) => {
+        err ? console.log(err) : console.log('Success! Logo created! in logo.svg');
+    });
+}
 
 // function to promt user for input using 'prompt' module from npm
 function promptUser() {
@@ -23,7 +54,7 @@ function promptUser() {
             type: 'list',
             name:'shape',
             message: 'Pick a shape for the logo',
-            choicees: ['square', 'circle', 'triangle'],
+            choices: ['square', 'circle', 'triangle'],
         },
         {
             type: 'input',
